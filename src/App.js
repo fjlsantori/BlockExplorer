20484a275c6633e4000e566a732a3283900c1616
fjlsantori <fjlsantori@gmail.com>
2023-09-15 17:31:53 +0200

    Gblock function

--- src/App.js
+++ src/App.js
@@ -26,11 +26,10 @@ function App() {
     async function getBlockNumber() {
       setBlockNumber(await alchemy.core.getBlockNumber());
     }
-
     getBlockNumber();
   });
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  return  <div className="App">Block Number: {blockNumber}</div>;
 }
 
 export default App;
